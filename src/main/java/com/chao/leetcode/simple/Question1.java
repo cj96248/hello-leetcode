39e2317a6e368440ef7ac8229ba2406177219a93
cj96248 <cj96248@sina.com>
2019-02-16 15:35:05 +0800

    项目初始化

---
+++ src/main/java/com/chao/leetcode/simple/Question1.java
@@ -0,0 +1,23 @@
+package com.chao.leetcode.simple;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int[] nums = {2,3,3,4};
+        int target = 7;
+        System.out.println(twoSum(nums, target));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+
+        for(int i = 0; i< nums.length; i++ ){
+            for(int j = i+1; j<nums.length; j++){
+                int num = nums[i] + nums[j];
+                System.out.println(num);
+                if(num == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
